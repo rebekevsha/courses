5a560508e88e0f9dc46dc47c51704823cf01ec5e
taras.rebekevsha <taras.rebekevsha@uklon.com.ua>
2020-01-23 20:56:29 +0200

    commit to dev

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -13,6 +13,7 @@ namespace LessonOne
                 Console.WriteLine($"Значения переменных уровнения {i}:  a = {(data.GetData(i)[0])}    b = {(data.GetData(i)[1])}    c = {(data.GetData(i)[2])}    d = {(data.GetData(i)[3])}");
                 double result = new Equations().Equation(data.GetData(i), i);
                 Console.WriteLine($"Результаты просчета уровнения {i}: " + result);
+                Console.WriteLine("Eah!!");
             }
             Console.ReadKey(); 
         }
