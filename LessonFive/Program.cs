c19c6b70c8ff4eccb847f8b8d0f011df58b3782c
Taras Rebekevsha <taras.rebekevsha@gmail.com>
2020-02-20 09:31:53 +0200

    5

---
+++ LessonFive/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LessonFive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student vasia = new Student() {FirstName = "ПЕТРО", LastName = "Вуйко" };
+            var nae = vasia.LastName.ToString();
+            Console.WriteLine(nae);
+        }
+    }
+    class Human
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Human(string FirstName)
+        {
+        }
+    }
+    class Student : Human
+    {
+        public string University { get; set; }
+        public string Сourse { get; set;}
+        public string Faculty { get; set; }
+
+        public Student() : base
+        {
+        }
+    }
+}
--- LessonFour/Program.cs
+++ LessonFour/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LessonFour
 {
@@ -7,8 +8,9 @@ namespace LessonFour
         static void Main(string[] args)
         {
             long[] numbers1 = { 10, 20, 15, 45, 78, 62, 42, 52, 20, 24, 25, 46, 2, 1, 2, -5, 20, 30, -45, 3 };
-            long[] numbers2 = { 5, 98, 54, 20, 1, 65, 53, 52, 41, 2, 6, 7, 5, 15, 2, 47, 95, 2, 78 ,24, 14};
+            long[] numbers2 = { 5, 98, 54, 20, 1, 65, 53, 52, 41, 2, 6, 7, 5, 15, 2, 47, 95, 2, 78, 24, 14 };
 
+                       
             //Lab - 6, Task - 2
             Console.WriteLine("Array 1 Before Sort : [{0}]", string.Join(", ", numbers1));
             InsertionSort(numbers1);
@@ -18,6 +20,7 @@ namespace LessonFour
             Console.WriteLine("Array 2 Before Sort : [{0}]", string.Join(", ", numbers2));
             BubbleSort(numbers2);
             Console.WriteLine("Bubble Sort : [{0}]", string.Join(", ", numbers2));
+            
         }
         static void InsertionSort(long[] numbers)
         {
